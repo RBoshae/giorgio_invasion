097acb85ea07aa931ce8e249b915dcccb9fdfc5b
rboshae <rwboshae@gmail.com>
2019-12-21 17:08:06 -0800

    Added alien movement behavior

--- src/game_functions.py
+++ src/game_functions.py
@@ -28,6 +28,18 @@ def create_alien(ai_settings, screen, aliens, alien_number, row_number):
 	aliens.add(alien)
 
 
+def check_fleet_edges(ai_settings, aliens):
+	"""Respond appropriately if any aliens have reached an edge."""
+	for alien in aliens.sprites():
+		if alien.check_edges():
+			change_fleet_direction(ai_settings, aliens)
+			break
+
+def change_fleet_direction(ai_settings, aliens):
+	"""Drop the entier fleet and change the fleet's direction"""
+	for alien in aliens.sprites():
+		alien.rect.y += ai_settings.fleet_drop_speed
+	ai_settings.fleet_direction *= -1
 
 def create_fleet(ai_settings, screen, ship, aliens):
 	"""Create a full fleet of aliens"""
@@ -95,6 +107,14 @@ def update_bullets(bullets):
 		if bullet.rect.bottom <= 0:
 			bullets.remove(bullet)
 		print (len(bullets))
+
+def update_aliens(ai_settings, aliens):
+	"""
+	Check if the fleet is at an edge,
+		and then update the positions of all aliens in the fleet.
+	"""
+	check_fleet_edges(ai_settings, aliens)
+	aliens.update()
 
 def update_screen(ai_settings, screen, ship, aliens, bullets):
 	"""Update images on the screen and flip to the new screen."""
--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,7 @@ class Settings():
 
 	def __init__(self):
 		"""Initialize the game's settings."""
+		
 		# Screen settings
 		self.screen_width = 1200
 		self.screen_height = 800
@@ -17,3 +18,9 @@ class Settings():
 		self.bullet_height = 15
 		self.bullet_color = 60, 60, 60
 		self.bullets_allowed = 3
+
+		# Alien settings
+		self.alien_speed_factor = 1
+		self.fleet_drop_speed = 10
+		# flee_direction of 1 represents reight; -1 represents left
+		self.fleet_direction = 1
